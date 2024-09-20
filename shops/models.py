f957d48d5de627742ac7ee50100e12b0dce7a7b7
Olga Menuhova <wowolgaivanova@gmail.com>
2024-09-20 15:03:42 +0300

    add info to README, add Shop class, ShopSerializer, ShopAdmin

--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+# Add my models here.
+
+class User(models.Model):
+
+    # создадим класс пользователя
+    
+    pass
+
+    
+class Shop(models.Model):
+
+    # create model Shop
+
+    name = models.OneToOneField(User, verbose_name='Пользователь', blank=True, null=True, on_delete=models.CASCADE) 
+    url = models.URLField(verbose_name='Ссылка', null=True, blank=True)
+
--- shops/serializers.py
+++ shops/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from models import Shop
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    # создадим сериализатор на основе модели Shop
+    class Meta:
+        model = Shop 
+        fields = ('id', 'name',) # поля, которые будут видны
